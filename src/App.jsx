36565c8cd5064a02eaf205f4669ce567bddf8004
Juan <juanmmplataforma@gmail.com>
2023-12-07 09:59:14 -0300

    Probando github

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 import ItemCaunt from "./ItemCaunt"
-import Navbar from "./Navbar"
+import Navbar from  "./Navbar"
 
 import { Button, ButtonGroup } from '@chakra-ui/react'
 import ItemListContainer from "./ItemListContainer"
