8abc484034e19cbd0882b4a12818107a34f65897
Juan <juanmmplataforma@gmail.com>
2024-01-26 00:04:27 -0300

    Firebase, ordenes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,63 @@
 import React, { useState } from 'react'
 import { Input, Button, Text } from '@chakra-ui/react'
 import Swal from 'sweetalert2'
+import { collection, addDoc, getFirestore } from 'firebase/firestore'
+import { CartContext } from '../context/ShoppingCartContext'
 
 
 const Form = () => {
+
+
+
+    const [nombre, setNombre] = useState("")
+    const [email, setEmail] = useState("")
+    const [orderId, setOrderId] = useState("")
+
+    const db = getFirestore()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("Enviando")
+        Swal.fire({
+            title: "Mensaje Aprobado",
+            text: "Su mensaje procederá a enviarse, de acuerdo?",
+            icon: "question"
+        });
+
+
+        addDoc(ordersCollection, order).then(({ id }) =>
+            setOrderId(id))
+    }
+
+
+    const order = {
+        cliente: { nombre, email },
+        //   items: { CartContext },
+
+    }
+
+    const ordersCollection = collection(db, "orden")
+
+
+
+
+    return (
+        <div>
+            <form action="" onSubmit={handleSubmit}>
+                <input type="text" placeholder='Nombre' onChange={(e) => setNombre(e.target.value)} value={nombre} />
+                <input type="email" placeholder='Correo' onChange={(e) => setEmail(e.target.value)} value={email} />
+                <button type="submit">Enviar </button>
+            </form>
+            <p>{orderId} </p>
+        </div>
+    )
+
+
+}
+
+
+
+{/* const Form = () => {
 
     const [nombre, setNombre] = useState("")
     const [email, setEmail] = useState("")
@@ -37,6 +91,6 @@ const Form = () => {
 
         </div>
     )
-}
+}   */}
 
 export default Form
